28fccd38099dd8bcd150bf410e595d15a11e093d
clover1420 <748883120@qq.com>
2022-05-22 11:08:52 +0800

    -fix : 去除测试配置

--- config.py
+++ config.py
@@ -12,11 +12,11 @@ push = {
 #签到token
 SignToken = {
     #好游快报 token
-    "gtfed":"4%7C0%7C14155929%7C57mB5Y2OLeWmgumjjg%3D%3D%7Ckb70BC9668C52C9E846EF562AD5560D6DC%7CGvSioRVxpvbWpvXf7jejpJZnGJ9rpvZUGipfpR7UGJZ%3D%251%7C0cdaa46bdc4b84e8aaa2ac8e476406ba&device=kb70BC9668C52C9E846EF562AD5560D6DC",
+    "gtfed":"",
     #miui历史版本签到
     "MiUI":{
         "switch":True,
-        "user":"748883120@qq.com",
-        "password":"luojiba666"
+        "user":"",
+        "password":""
     }
 }
